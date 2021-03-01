e72c3c6ea2911979912f2e51a295da147ba04055
zmarkan <zan@circleci.com>
2021-03-01 04:35:15 +0000

    updated all them deps. Tests passing on AS 4.1.2

--- app/src/androidTest/java/com/example/android/architecture/blueprints/todoapp/util/DataBindingIdlingResource.kt
+++ app/src/androidTest/java/com/example/android/architecture/blueprints/todoapp/util/DataBindingIdlingResource.kt
@@ -102,12 +102,3 @@ fun DataBindingIdlingResource.monitorActivity(
         this.activity = it
     }
 }
-
-/**
- * Sets the fragment from a [FragmentScenario] to be used from [DataBindingIdlingResource].
- */
-fun DataBindingIdlingResource.monitorFragment(fragmentScenario: FragmentScenario<out Fragment>) {
-    fragmentScenario.onFragment {
-        this.activity = it.requireActivity()
-    }
-}

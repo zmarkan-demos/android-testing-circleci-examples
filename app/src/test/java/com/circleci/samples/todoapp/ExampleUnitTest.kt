73c32a32d4d526e5c3e0c2f6bc0a662927fdd771
Zan Markan <zan@circleci.com>
2021-10-08 11:13:13 +0100

    fail an example test

--- app/src/test/java/com/circleci/samples/todoapp/ExampleUnitTest.kt
+++ app/src/test/java/com/circleci/samples/todoapp/ExampleUnitTest.kt
@@ -14,4 +14,9 @@ class ExampleUnitTest {
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
     }
+    
+    @Test
+    fun addition_shouldFail() {
+        assertEquals(5, 2 + 2)
+    }
 }

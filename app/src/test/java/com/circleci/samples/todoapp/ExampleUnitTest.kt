013c42589fcc8ffdc734140758bc20f7471bcece
zmarkan <zan@circleci.com>
2021-11-11 09:51:08 +0000

    un-fail a failing test

--- app/src/test/java/com/circleci/samples/todoapp/ExampleUnitTest.kt
+++ app/src/test/java/com/circleci/samples/todoapp/ExampleUnitTest.kt
@@ -14,9 +14,10 @@ class ExampleUnitTest {
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
     }
-    
-    @Test
-    fun addition_shouldFail() {
-        assertEquals(5, 2 + 2)
-    }
+
+// Sample failing test :)
+//    @Test
+//    fun addition_shouldFail() {
+//        assertEquals(5, 2 + 2)
+//    }
 }
